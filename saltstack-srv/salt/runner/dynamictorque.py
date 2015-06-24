a4c7f7abd7670d5c6828b40604919138b3ebfc88
Shunde Zhang <shunde.p.zhang@gmail.com>
2015-06-24 10:35:14 +0930

    runner

---
+++ saltstack-srv/salt/runner/dynamictorque.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+import salt.pillar
+import salt.wheel
+
+def procee_minion_request(minion_id):
+    saltenv = 'base'
+    id_, grains, _ = salt.utils.minions.get_minion_data(minion, __opts__)
+    if grains is None:
+        grains = {'fqdn': minion}
+
+    for key in kwargs:
+        if key == 'saltenv':
+            saltenv = kwargs[key]
+        else:
+            grains[key] = kwargs[key]
+
+    pillar = salt.pillar.Pillar(
+        __opts__,
+        grains,
+        id_,
+        saltenv)
+
+    compiled_pillar = pillar.compile_pillar()
+
+    try:
+        process = Popen(shlex.split(compiled_pillar["get_starting_instance_names_cmd"]), stdout=PIPE)
+        pipe = process.stdout
+        output = pipe.readlines()
+        process.wait()
+        returncode = process.returncode
+    except:
+        print "Dynamic Torque is not running."
+    else:
+        wheel = salt.wheel.Wheel(__opts__)
+        if minion_id in output:
+            wheel.call_func('key.accept', match=minion_id)
+        else:
+            wheel.call_func('key.reject', match=minion_id)
+        
